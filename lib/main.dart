7671877a00161822426bb0fce3791a7df3b3dae8
Jose_Maria <josemariazapataq2@gmail.com>
2024-05-02 13:50:50 -0400

    arreglado

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:parkfinder/views/login_screen.dart';
 
 void main() {
-  runApp(const LoginPage());
+  runApp( MyApp());
 }
+class MyApp extends StatelessWidget {
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: LoginPage(),
+    );
+  }
+}
